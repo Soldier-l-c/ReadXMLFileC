31c58a9c9e8bfe5190cccd87f1dfe5852cdf2d5f
liuquanchang <1870056213@qq.com>
2020-08-07 17:38:50 +0800

    读取配置文件

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,18 @@
 #include<stdio.h>
 #include <string>
 #include <iostream>
+#include <tchar.h>
+#include <windows.h>
 using namespace std;
-#define XMLFilePath "C:\\Users\\liuquanchang\\source\\repos\\ReadXMLFileC\\ReadXMLFileC\\Debug\\MassSpectrumLab.xml"
+//"C:\\Users\\liuquanchang\\Desktop\\NeedsXML.xml"
+//"C:\\Users\\liuquanchang\\source\\repos\\ReadXMLFileC\\ReadXMLFileC\\Debug\\MassSpectrumLab.xml"
+#define XMLFilePath "C:\\Users\\liuquanchang\\Desktop\\NeedsXML.xml"
 
 void printSchoolXml();
 void readSchoolXml();
 
 int main(int argc, char** argv) {
-    printSchoolXml();
+    //printSchoolXml();
     readSchoolXml();
     return 0;
 }
@@ -43,52 +47,59 @@ void readSchoolXml()
     }
     else
     {
-        TiXmlElement* mark_id = MassSpectrumLab->FirstChildElement(); // xml的id 根节点下第一个子节点
-        TiXmlElement* Chinese_name = mark_id->NextSiblingElement();        // mark_id 下一个子节点
-        TiXmlElement* English_name = Chinese_name->NextSiblingElement();   // Chinese_name 下一个子节点
-        TiXmlElement* Parent_ion = English_name->NextSiblingElement();   // English_name 下一个子节点
-        TiXmlElement* DaughterNum = Parent_ion->NextSiblingElement();     // Parent_ion 下一个子节点
-        TiXmlElement* Daughter_Ion = DaughterNum->NextSiblingElement();    // DaughterNum 下一个子节点
-        TiXmlElement* Daughter = Daughter_Ion->FirstChildElement();//获得Daughter_ion的第一个子元素
+        //需求id
+        TiXmlElement* requirement_id = MassSpectrumLab->FirstChildElement(); // xml的id 根节点下第一个子节点
         int temp = 0;
 
-        while (mark_id != NULL)
+        while (requirement_id != NULL)
         {
             temp++; 
             cout << temp << endl;
-            Chinese_name = mark_id->NextSiblingElement();        // mark_id 下一个子节点
-            English_name = Chinese_name->NextSiblingElement();   // Chinese_name 下一个子节点
-            Parent_ion = English_name->NextSiblingElement();   // English_name 下一个子节点
-            DaughterNum = Parent_ion->NextSiblingElement();     // Parent_ion 下一个子节点
-            Daughter_Ion = DaughterNum->NextSiblingElement();    // DaughterNum 下一个子节点
-            Daughter = Daughter_Ion->FirstChildElement();    //获得Daughter_ion的第一个子元素
-            string TypeMark_id = mark_id->Value();
-            string Mark_idValue = mark_id->GetText();
-            cout << TypeMark_id << " : " << Mark_idValue << std::endl;
+            //需求名称
+            TiXmlElement* requirement_name = requirement_id->NextSiblingElement();        // requirement_id 下一个子节点
+            //需求地址
+            TiXmlElement* requirement_address = requirement_name->NextSiblingElement();   // requirement_name 下一个子节点
+            //代码地址
+            TiXmlElement* code_address = requirement_address->NextSiblingElement();   // requirement_address 下一个子节点
+            //本地地址
+            TiXmlElement* local_address = code_address->NextSiblingElement();     // code_address 下一个子节点
+            //bug列表
+            TiXmlElement* bug_list = local_address->NextSiblingElement();    // local_address 下一个子节点
+            //bug列表的第一个bug
+            TiXmlElement* bug = bug_list->FirstChildElement();//获得bug_list的第一个子元素
 
-            string TypeChinese_name = Chinese_name->Value();
-            string Chinese_nameValue = Chinese_name->GetText();
+            //需求id
+            string TypeMark_id = requirement_id->Value();
+            string Mark_idValue = requirement_id->GetText();
+            cout << TypeMark_id << " : " << Mark_idValue << std::endl;
+            
+            //需求名称
+            string TypeChinese_name = requirement_name->Value();
+            string Chinese_nameValue = requirement_name->GetText();
             cout << TypeChinese_name << " : " << Chinese_nameValue << std::endl;
 
-            string TypeEnglish_name = English_name->Value();
-            string English_nameValue = English_name->GetText();
-            cout << TypeEnglish_name << " : " << TypeEnglish_name << std::endl;
+            //需求地址
+            string TypeEnglish_name = requirement_address->Value();
+            string English_nameValue = requirement_address->GetText();
+            cout << TypeEnglish_name << " : " << English_nameValue << std::endl;
 
-            string TypeParent_ion = Parent_ion->Value();
-            string Parent_ionValue = Parent_ion->GetText();
-            cout << TypeParent_ion << " : " << TypeParent_ion << std::endl;
+            //代码地址
+            string TypeParent_ion = code_address->Value();
+            string Parent_ionValue = code_address->GetText();
+            cout << TypeParent_ion << " : " << Parent_ionValue << std::endl;
 
-            string TypeDaughterNum = DaughterNum->Value();
-            string DaughterNumValue = DaughterNum->GetText();
+            //本地地址
+            string TypeDaughterNum = local_address->Value();
+            string DaughterNumValue = local_address->GetText();
             cout << TypeDaughterNum << " : " << DaughterNumValue << std::endl;
 
-            for (; Daughter != NULL; Daughter = Daughter->NextSiblingElement()) {
-                string contactType = Daughter->Value();
-                string contactValue = Daughter->GetText();
+            //bug列表
+            for (; bug != NULL; bug = bug->NextSiblingElement()) {
+                string contactType = bug->Value();
+                string contactValue = bug->GetText();
                 cout << contactType << " : " << contactValue << std::endl;
             }
-            mark_id = Daughter_Ion->NextSiblingElement();
+            requirement_id = bug_list->NextSiblingElement();
         }
-        cout << temp << endl;
     }
 }
